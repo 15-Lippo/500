6ea9c650a9db3eb6a2d2b13ec9bc76d31dfb2bc9
Lisprocoin <83799681+15-Lippo@users.noreply.github.com>
2025-01-27 22:22:40 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import WalletWrapper from './WalletWrapper';
+import './styles.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <WalletWrapper />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
