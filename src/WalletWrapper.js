431786431176fb6ac2687fd453d63c5f6cf8d156
Lisprocoin <83799681+15-Lippo@users.noreply.github.com>
2025-01-27 22:23:56 +0100

    Create WalletWrapper.js

---
+++ src/WalletWrapper.js
@@ -0,0 +1,30 @@
+import React, { useMemo } from 'react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+import App from './App';
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+function WalletWrapper() {
+    const network = WalletAdapterNetwork.Devnet; // Usa 'Devnet' per test
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+    const wallets = useMemo(
+        () => [
+            new PhantomWalletAdapter(),
+        ],
+        []
+    );
+
+    return (
+        <WalletProvider wallets={wallets} autoConnect endpoint={endpoint}>
+            <WalletModalProvider>
+                <App />
+            </WalletModalProvider>
+        </WalletProvider>
+    );
+}
+
+export default WalletWrapper;
